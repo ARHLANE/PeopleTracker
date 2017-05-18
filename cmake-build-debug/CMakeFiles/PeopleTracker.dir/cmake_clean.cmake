4b956e35491386e61e7914fb48d728abacb3b1ab
dilin993 <dilin.dampahalage@gmail.com>
2017-05-18 23:33:39 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PeopleTracker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PeopleTracker.dir/main.cpp.o"
+  "CMakeFiles/PeopleTracker.dir/hungarian.cpp.o"
+  "CMakeFiles/PeopleTracker.dir/PersonTrack.cpp.o"
+  "PeopleTracker.pdb"
+  "PeopleTracker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PeopleTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
